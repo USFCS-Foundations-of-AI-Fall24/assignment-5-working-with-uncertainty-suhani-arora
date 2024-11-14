f3d5cdc5360b8a3a1bf891127c209060b6d930db
Suhani <suhaniarora26@gmail.com>
2024-11-13 23:22:17 -0800

    P3 - load+generate+main

--- HMM.py
+++ HMM.py
@@ -27,9 +27,6 @@ class HMM:
         e.g. {'happy': {'silent': '0.2', 'meow': '0.3', 'purr': '0.5'},
               'grumpy': {'silent': '0.5', 'meow': '0.4', 'purr': '0.1'},
               'hungry': {'silent': '0.2', 'meow': '0.6', 'purr': '0.2'}}"""
-
-
-
         self.transitions = transitions
         self.emissions = emissions
 
@@ -38,13 +35,43 @@ class HMM:
         """reads HMM structure from transition (basename.trans),
         and emission (basename.emit) files,
         as well as the probabilities."""
-        pass
+        self.transitions = {}
+        with open(f"{basename}.trans", "r") as file_trans:
+            for line in file_trans:
+                parts = line.strip().split()
+                if parts[0] not in self.transitions:
+                    self.transitions[parts[0]] = {}
+                self.transitions[parts[0]][parts[1]] = float(parts[2])
+
+        self.emissions = {}
+        with open(f"{basename}.emit", "r") as file_emit:
+            for line in file_emit:
+                parts = line.strip().split()
+                if parts[0] not in self.emissions:
+                    self.emissions[parts[0]] = {}
+                self.emissions[parts[0]][parts[1]] = float(parts[2])
 
 
    ## you do this.
     def generate(self, n):
         """return an n-length Sequence by randomly sampling from this HMM."""
-        pass
+        curr_state = random.choices(list(self.transitions['#'].keys()), weights=list(self.transitions['#'].values()))[0]
+        stateseq = [curr_state]
+        outputseq = []
+
+        for _ in range(n):
+            if curr_state in self.emissions:
+                emission = random.choices(list(self.emissions[curr_state].keys()), weights=list(self.emissions[curr_state].values()))[0]
+                outputseq.append(emission)
+
+            if curr_state in self.transitions:
+                next_state = random.choices(list(self.transitions[curr_state].keys()), weights=list(self.transitions[curr_state].values()))[0]
+                stateseq.append(next_state)
+                curr_state = next_state
+            else:
+                break
+
+        return Sequence(stateseq, outputseq)
 
     def forward(self, sequence):
         pass
@@ -62,6 +89,21 @@ class HMM:
     ## hidden states using the Viterbi algorithm.
 
 
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("basename", help="HMM file name without the extension")
+    p.add_argument("--generate", type=int, help="input length")
+    args = p.parse_args()
+
+    h = HMM()
+    h.load(args.basename)
+
+    if args.generate:
+        sequence = h.generate(args.generate)
+        print(sequence)
+
+if __name__ == "__main__":
+    main()
 
 
 
